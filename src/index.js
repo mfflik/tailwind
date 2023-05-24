1388c5d1316f139f94c1870089f8ea55f1c0557b
M Fauzan F <muh.fauzan.f@gmail.com>
2023-05-24 13:21:15 +0700

    react from strach

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./app";
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(
+   <App />
+);
