97d423688fa49ce5c4a1be8991ccb8459b882332
M Fauzan F <muh.fauzan.f@gmail.com>
2023-05-24 13:57:12 +0700

    Basic Tailwind React

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./app.css";
+import Button from "./components/button/button";
 
 function App() {
- return (
-   <div className="App">
-    Hello World
-   </div>
- );
+  return (
+    <div>
+      <h1 className="text-primary text-4xl font-bold">
+        Hello world! I am using React
+      </h1>
+      <Button/>
+    </div>
+  );
 }
+
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
- template: "./src/index.html",
- filename: "./index.html"
-});module.exports = {mode: 'development',
+  template: "./public/index.html",
+  filename: "./index.html",
+  favicon: './public/favicon.ico',
+});
+module.exports = {
+  mode: "development",
   module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: "babel-loader"
-   }
- },
-  {
-   test: /\.css$/,
-   use: ["style-loader", "css-loader"]
-  }
-]},
- plugins: [htmlPlugin]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+    },
+    ],
+  },
+  plugins: [htmlPlugin],
 };
